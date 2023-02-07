22ffe1643d2c22c4267f3ef2987defe0e5ac1a4e
matveySV <ihtmatvienko@gmail.com>
2023-02-07 16:39:10 +0200

    first commit - empty project

---
+++ src/main/java/com/matviienko/travel_agency_project/TravelAgencyProjectApplication.java
@@ -0,0 +1,13 @@
+package com.matviienko.travel_agency_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelAgencyProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TravelAgencyProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/matviienko/travel_agency_project/TravelAgencyProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matviienko.travel_agency_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelAgencyProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
